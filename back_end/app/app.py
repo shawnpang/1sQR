1da9513c72ce932a3dafe926b4b5da8596e1feb2
bryankou <bryanwkou@gmail.com>
2020-09-25 21:25:56 -0700

    added image saving

--- back_end/app/app.py
+++ back_end/app/app.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify,send_from_directory
 import os 
-from firebase_admin import credentials, firestore
+from flask_restful import Resource, Api
+from werkzeug.utils import secure_filename
+from pathlib import Path
+import uuid 
+
+
+Path(__file__).parent
+
+
+
+UPLOAD_DIRECTORY = "./data"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
 
 app = Flask(__name__)
 
+@app.route('/upload', methods=["POST"])
+def upload():
+    if request.files:
+        pic = request.files["image"]
+        fileName = secure_filename(pic.filename)
+        pic.save(os.path.join(UPLOAD_DIRECTORY, fileName))
+    
+        return jsonify({"Uploaded":fileName})
+                
+@app.route('/getImage/<path:path>', methods =["GET"])
+def getImage(path):
+    unique_filename = str(uuid.uuid4())
+    print(unique_filename)
+    return send_from_directory(UPLOAD_DIRECTORY,path, as_attachment = True)
+                
+@app.route('/')
+def main():
+    return render_template("upload.html")
 
-cred = credentials.Certificate("/sbuhacks-1sqr-firebase-adminsdk-66hto-3981f9b275.json")
-firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+
 
 
 @app.route("/upload-image", methods=["GET","POST"])
